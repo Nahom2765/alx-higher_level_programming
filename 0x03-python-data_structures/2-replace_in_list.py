e8e9d5a57474bad8462af472cc8cb110e6e60dbd
Nahom2765 <19nahoma@gmail.com>
2022-07-03 07:07:22 -0700

    list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,4 +2,4 @@
 def replace_in_list(my_list, idx, new_element):
     if idx >= 0 and idx < len(my_list):
         my_list[idx] = new_element
-    return 
+    return my_list
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
+    if my_string and type(my_string) == str:
         withoutc = ""
         for char in my_string:
             if char != 'c' and char != 'C':
                 withoutc += char
-    return withoutc
+        return withoutc
+    return my_string
